d0dcd93d9bd2f18ed40c0a54669ec229670e67b9
Nobleyyy <156829456+Nobleyyy@users.noreply.github.com>
2024-11-11 15:02:22 +0000

    Update Commented.py

--- Commented.py
+++ Commented.py
@@ -207,7 +207,7 @@ def CheckIfUserInputValid(UserInput):
     if re.search("^([0-9]+[\\+\\-\\*\\/])+[0-9]+$", UserInput) is not None:
         # ^ Start of expression
         # [] A set of characters
-        # 0-9 All digits from 0 to 9
+        # 0-9 All digits
         # + One or more Occurrences
         # \\+\\-\\*\\/ plus or minus or asterisk or slash            \\+\\ exclude special function to just use as a character
         # $ End of expression
